04843eb5d28ddb35381eb722923be439c4eb2066
Akiva Cohen <Akivaecohen@gmail.com>
2024-12-01 15:18:04 -0500

    v1.1.3 scaling change

--- ti-note/src/App.java
+++ ti-note/src/App.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 public class App {
     public static void main(String[] args) throws Exception {
+        UIManager.put("swing.boldMetal", Boolean.FALSE);  // Disable bold metal rendering
+        System.setProperty("awt.useSystemAAFontSettings", "on");
+
         if (Popup.startUpPopUp()) {
             GUI.standardUI();
         } else {    
