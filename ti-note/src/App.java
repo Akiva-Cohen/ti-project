c76d55c35448828498c0686f308408565540b1bd
Akiva Cohen <Akivaecohen@gmail.com>
2024-12-01 15:26:02 -0500

    v1.1.4 more rendering

--- ti-note/src/App.java
+++ ti-note/src/App.java
@@ -4,10 +4,13 @@ public class App {
     public static void main(String[] args) throws Exception {
         UIManager.put("swing.boldMetal", Boolean.FALSE);  // Disable bold metal rendering
         System.setProperty("awt.useSystemAAFontSettings", "on");
+        System.setProperty("sun.java2d.dpiaware", "true");
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
 
         if (Popup.startUpPopUp()) {
             GUI.standardUI();
-        } else {    
+        } else {   
             GUI.textNoteWindow();
         }
     }
