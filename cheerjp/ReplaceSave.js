6e206f639df1a1d7abff4834b88ca1bd757d1bd7
Akiva Cohen <Akivaecohen@gmail.com>
2024-12-17 11:56:46 -0500

    removed printing

--- cheerjp/ReplaceSave.js
+++ cheerjp/ReplaceSave.js
@@ -1,12 +1,10 @@
-console.log("replacer");
 async function Java_GUI_cheerpD(lib, str) {
-    console.log("called")
     //make the link
     var content = str;  
     var blob = new Blob([content], {type:'text/plain'});
     var link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
-    link.download = "testfile.txt";
+    link.download = "ti-notes-program.txt";
     //click the link
     document.body.appendChild(link);
     link.click();
